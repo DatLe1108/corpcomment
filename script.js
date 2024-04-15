5c9d050bae5d99b2093d8b979b04ea8f5dcd1f98
Dat Le <datle1108@gmail.com>
2024-04-15 22:32:52 +0200

    Add functionality

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const formEl = document.querySelector(".form");
 const textareaEl = document.querySelector(".form__textarea");
 const counterEl = document.querySelector(".counter");
+const feedbackListEl = document.querySelector(".feedbacks");
+const submitBtnEl = document.querySelector(".submit-btn");
 
 const inputHandler = () => {
   const maxNurChars = 150;
@@ -16,6 +18,51 @@ const submitHandler = (event) => {
 
   // get text from tex area
   const text = textareaEl.value;
+
+  // validate text
+
+  if (text.includes("#") && text.length >= 5) {
+    formEl.classList.add("form--valid");
+    setTimeout(() => {
+      formEl.classList.remove("form--valid");
+    }, 2000);
+  } else {
+    formEl.classList.add("form--invalid");
+    setTimeout(() => {
+      formEl.classList.remove("form--invalid");
+    }, 2000);
+    textareaEl.focus();
+    return;
+  }
+
+  const hashtag = text.split(" ").find((word) => word.includes("#"));
+  const company = hashtag.substring(1);
+  const badgeLetter = company.substring(0, 1).toUpperCase();
+  const upvoteCount = 0;
+  const daysAgo = 0;
+
+  const feedBackItemHTML = `<li class="feedback">
+  <button class="upvote">
+      <i class="fa-solid fa-caret-up upvote__icon"></i>
+      <span class="upvote__count">${upvoteCount}</span>
+  </button>
+  <section class="feedback__badge">
+      <p class="feedback__letter">${badgeLetter}</p>
+  </section>
+  <div class="feedback__content">
+      <p class="feedback__company">${company}</p>
+      <p class="feedback__text">${text}</p>
+  </div>
+  <p class="feedback__date">${daysAgo === 0 ? "NEW" : `${daysAgo}d`}</p>
+</li>`;
+
+  feedbackListEl.insertAdjacentHTML("beforeend", feedBackItemHTML);
+
+  textareaEl.value = "";
+
+  submitBtnEl.blur();
+
+  counterEl.textContent = 150;
 };
 
 formEl.addEventListener("submit", submitHandler);
